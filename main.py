013a3e838df15574caeecdf7e737490fda037b96
mel-th <meleyth@gmail.com>
2025-05-01 18:03:57 -0400

    fixed layers & infinite loop - still needs work

--- main.py
+++ main.py
@@ -50,7 +50,8 @@ class FetchStockData:
         try:
             bars = stock_client.get_stock_bars(self.request_params)
             df = bars.df #dataframe from pandas
-            print(df)
+            return df
+            #print(df)
 
         except Exception as e:
             print(f"Error fetching data: {str(e)}")
@@ -81,92 +82,93 @@ class PrepareData:
         
         # transforming then applies this learned process to new data
         
-        train_data = self.scaler.fit_transform(train_data) #uses both fit & transform
+        self.train_data = self.scaler.fit_transform(self.train_data) #uses both fit & transform
         #The fit(data) method is used to compute the mean and std dev 
         # for a given feature to be used further for scaling.
-        test_data = self.scaler.transform(test_data)
+        self.test_data = self.scaler.transform(self.test_data)
         # transform(data) method is used to perform scaling using 
         # mean and std dev calculated using the .fit() method.
         
 class CreateLSTMModel:
-    #this class creates the LSTM model
-    
-    def __init__(self, train_data, test_data, columns, neuron_num):
+    def __init__(self, train_data, test_data, column_num, neuron_num):
         self.train_data = train_data
         self.test_data = test_data
+        self.column_num = column_num
+        self.neuron_num = neuron_num
         self.stock_model = Sequential()
-        self.train_data_shape = columns
-        self.neuron_num =  neuron_num #starting here! can change later
-    
-    def one_step_ahead_prediction(self):
-        #this method creates a one step ahead prediction
-        #it takes the last 60 days of training data and uses it to predict the next day's closing price
-        pass
-    def multi_step_ahead_prediction(self):
-        #this method creates a multi step ahead prediction
-        #it takes the last # days of training data and uses it to predict the next 
-        # Y number of days of closing prices
-        pass
-    
-    def create_model(self, column_num): #not explicity called, but initial layer has
-        #7 features for the 7 columns in the training data relfected in the input_shape
-        #input layer has NO neurons it just passes the data to the first LSTM layer
 
-        self.stock_model.add(Dense(32,activation="relu", input_shape=(column_num,), name="layer_1"))
+    def create_model(self, column_num):
+        # Reshape input data for LSTM [samples, timesteps, features]
+        self.train_data = self.train_data.reshape((self.train_data.shape[0], 1, self.train_data.shape[1]))
+        self.test_data = self.test_data.reshape((self.test_data.shape[0], 1, self.test_data.shape[1]))
+
+        # First LSTM layer
+        self.stock_model.add(LSTM(
+            units=self.neuron_num,
+            return_sequences=True,
+            input_shape=(1, self.column_num),
+            name="lstm_1"
+        ))
+
+        # Additional LSTM layers with decreasing number of neurons
         i = 2
-        neuron_loop = self.neuron_num
-        while i > 1: #allows for dynamic neuron number when fine-tuning later!
-            neuron_loop = neuron_loop/2
+        neuron_loop = self.neuron_num // 2
+        while neuron_loop >= 2:
+            self.stock_model.add(LSTM(
+                units=neuron_loop,
+                return_sequences=(neuron_loop > 2),  # Only return sequences if not the last LSTM layer
+                name=f"lstm_{i}"
+            ))
+            neuron_loop = neuron_loop // 2
             i += 1
-            self.stock_model.add(Dense(neuron_loop,self.neuron_num,activation="relu", name=f"layer_{i}"))
-        self.stock_model.add(Dense(1,activation="relu", name="output_layer"))
-        
-        """           
-        self.stock_model.add(Dense(16,activation="relu", name="layer_2"))
-        self.stock_model.add(Dense(8,activation="relu", name="layer_3"))
-        self.stock_model.add(Dense(4,activation="relu", name="layer_4"))
-        self.stock_model.add(Dense(2,activation="relu", name="layer_5")) """
-        
-        model_summary = self.stock_model.summary()
-        print(model_summary)
-        
-        
-        
-        
-        
-        
-        
-        
+
+        # Final Dense layer for prediction
+        self.stock_model.add(Dense(1, activation="linear", name="output_layer"))
+
+        # Print model summary
+        self.stock_model.summary()
+
 def main():
-    config = DataConfig()  # Create an instance of DataConfig
+    config = DataConfig()
     fetch_stock_data = FetchStockData(config.symbol, config.startTraining, config.now, config.stock_client)
     df_time_series = fetch_stock_data.fetch_data(config.stock_client)
-    prepare_data = PrepareData(df_time_series)
-    neuron_num = 32  # starting here! can change later will also change organization of this later! to avoid over computation and overfitting
-    create_model = CreateLSTMModel(prepare_data.train_data, prepare_data.test_data, fetch_stock_data.column_num, neuron_num)
-    
-    
-    
-    """ multi_day_prediction = input("Do you want to predict for multiple days? (y/n)")
-    if multi_day_prediction == "y" or multi_day_predictiction == "Y" or multi_day_prediction == "yes" or multi_day_prediction == "Yes" or multi_day_prediction == "True":
-        multi_day_prediction = True
-        print("How many days would you like to predict for?")
-        num_days = int(input())
+    print("Raw data fetched:\n", df_time_series.head())
 
-    elif multi_day_prediction == "n" or multi_day_predictiction == "N" or multi_day_prediction == "no" or multi_day_prediction == "No" or multi_day_prediction == "False":
-        multi_day_prediction = False
-    else:
-        print("Invalid input")
-        return
-    if multi_day_prediction:
-        multi_step_ahead_prediction = CreateLSTMModel(prepare_data.train_data, prepare_data.test_data)
-        multi_step_ahead_prediction.multi_step_ahead_prediction()
-    else: #predicting for tommorrow
-        one_step_ahead_prediction = CreateLSTMModel(prepare_data.train_data, prepare_data.test_data)
-        one_step_ahead_prediction.one_step_ahead_prediction()
+    prepare_data = PrepareData(df_time_series)
+    prepare_data.prepare_data()
+    train_data, test_data = prepare_data.split_data()
+    prepare_data.train_data = train_data
+    prepare_data.test_data = test_data
+    prepare_data.scale_data()
+
+    neuron_num = 32
+    create_model = CreateLSTMModel(prepare_data.train_data, prepare_data.test_data, fetch_stock_data.column_num, neuron_num)
+    create_model.create_model(fetch_stock_data.column_num)
     
+    # Compile the model
+    create_model.stock_model.compile(
+        optimizer="adam",
+        loss="mean_squared_error",
+        metrics=["mae"]
+    )
     
- """
+    # Train the model
+    history = create_model.stock_model.fit(
+        create_model.train_data,
+        create_model.train_data[:, -1],  # 
+        epochs=50,
+        batch_size=32,
+        validation_split=0.2,
+        verbose=1
+    )
+    
+    # Evaluate the model
+    test_loss, test_mae = create_model.stock_model.evaluate(
+        create_model.test_data,
+        create_model.test_data[:, -1]
+    )
+    print(f"Test Loss: {test_loss:.4f}")
+    print(f"Test MAE: {test_mae:.4f}")
 
 
 if __name__ == "__main__":
