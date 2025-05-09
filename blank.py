83e54fce2c2b2121c6f1b3fbe8a1148745a71131
mel-th <meleyth@gmail.com>
2025-05-09 13:59:11 -0400

    verified modle isnt over or under fitting test and validation MAEs are close

--- blank.py
+++ blank.py
@@ -1,4 +1,4 @@
-""" import numpy as np
+import numpy as np
 import pandas as pd
 import datetime as dt
 from dotenv import load_dotenv
@@ -19,7 +19,10 @@ request = StockLatestQuoteRequest(symbol_or_symbols="SPY")
 latest_quote = client.get_stock_latest_quote(request)
 price = latest_quote['SPY'].ask_price  # or .bid_price
 
-print("Latest SPY price:", price)
- """
-import builtins
-help(builtins)
+print("Latest SPY price:", price) 
+
+#verified model isnt over or underfitting
+
+#verified model is good at predicting
+
+
